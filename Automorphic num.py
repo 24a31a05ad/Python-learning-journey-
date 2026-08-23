4d763600147ddc207e4ca47f9bf2d506f3193241
Tota Amruta valli <24a31a05ad@pragati.ac.in>
2026-08-23 16:26:31 +0530

    Add files via upload

---
+++ Automorphic num.py
@@ -0,0 +1,10 @@
+# Check Automorphic Number
+
+num = int(input("Enter a number: "))
+
+square = num * num
+
+if str(square).endswith(str(num)):
+    print("Automorphic Number")
+else:
+    print("Not an Automorphic Number")
